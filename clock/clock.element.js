5e203cc863fe095eb30523a2fbeb0768bfc85ea6
inhuman <inhuman.en@gmail.com>
2018-01-13 19:16:41 +0200

    fixed settable time, deps, property-attribute sync

--- clock/clock.element.js
+++ clock/clock.element.js
@@ -1,86 +1,99 @@
-(function () {
+(function() {
     let tpl = document.currentScript.ownerDocument.querySelector('#clockTpl');
 
     class XClock extends HTMLElement {
+        static get observedAttributes() {
+            return ['time'];
+        }
 
-        static get observedAttributes() {return ['time']; }
-
-        static get tickSpan () {
+        static get tickSpan() {
             return 1000;
         }
 
-        get time () {
+        get time() {
             return this.milliseconds;
         }
 
-        set time (value) {
-            let valueAsDate = new Date(value);
+        set time(value) {
+            if (isNaN(value)) {
+                throw new Error('time should be a numeric value');
+            } else {
+                let valueAsDate = new Date(value);
 
-            this.milliseconds = value;
-            this.hours = valueAsDate.getHours();
-            this.minutes = valueAsDate.getMinutes();
-            this.seconds = valueAsDate.getSeconds();
+                this.milliseconds = value;
+                this.hours = valueAsDate.getHours();
+                this.minutes = valueAsDate.getMinutes();
+                this.seconds = valueAsDate.getSeconds();
+            }
         }
 
         constructor() {
             super();
         }
 
-        _appendTpl () {
-            let root = this.attachShadow({ mode: "open" });
+        _appendTpl() {
+            let root = this.attachShadow({ mode: 'open' });
             let tplHtml = document.importNode(tpl.content, true);
 
             root.appendChild(tplHtml);
         }
 
-        _tick () {
+        _tick() {
             this.time = this.milliseconds + XClock.tickSpan;
 
-            let secondRotation = 6*this.seconds;
-            let minuteRotation = 6*this.minutes;
-            let hourRotation = (360/24)*this.hours;
+            let secondRotation = 6 * this.seconds;
+            let minuteRotation = 6 * this.minutes;
+            let hourRotation = 360 / 12 * this._24To12(this.hours);
 
-            this.arrows.second.setAttribute("transform", `rotate(${secondRotation} 150 150)`);
-            this.arrows.minute.setAttribute("transform", `rotate(${minuteRotation} 150 150)`);
-            this.arrows.hour.setAttribute("transform", `rotate(${hourRotation} 150 150)`);
+            this.arrows.second.setAttribute(
+                'transform',
+                `rotate(${secondRotation} 150 150)`
+            );
+            this.arrows.minute.setAttribute(
+                'transform',
+                `rotate(${minuteRotation} 150 150)`
+            );
+            this.arrows.hour.setAttribute(
+                'transform',
+                `rotate(${hourRotation} 150 150)`
+            );
 
-            //todo: fire tick event
+            this._fireEvent('tick');
         }
-    
+
+        _24To12(hours) {
+            return hours % 12;
+        }
+
+        _fireEvent(name) {
+            var evt = new Event(name, { bubbles: true, cancelable: false });
+            this.dispatchEvent(evt);
+        }
+
         connectedCallback() {
             this._appendTpl();
 
             this.arrows = {
-                hour: this.shadowRoot.querySelector(".hour-arrow"),
-                minute: this.shadowRoot.querySelector(".minute-arrow"),
-                second: this.shadowRoot.querySelector(".second-arrow")
+                hour: this.shadowRoot.querySelector('.hour-arrow'),
+                minute: this.shadowRoot.querySelector('.minute-arrow'),
+                second: this.shadowRoot.querySelector('.second-arrow')
             };
 
-            this.time = Date.now();
+            this.time = this.time !== undefined ? this.time : Date.now();
 
             this._tick();
             this.timerId = setInterval(this._tick.bind(this), XClock.tickSpan);
         }
-    
+
         disconnectedCallback() {
             clearInterval(this.timerId);
-            console.log("disconnected!");
+            console.log('disconnected!');
         }
 
-        attributeChangedCallback (name, oldValue, newValue) {
-            if (name = "time") {
-                let newTime = +newValue
-
-                if (isNaN(newTime)) {
-                    throw new Error("time should be a numeric value");
-                } else {
-                    this.time = newTime;
-                    this._tick();
-                }
-            }
+        attributeChangedCallback(name, oldValue, newValue) {
+            this.time = parseInt(newValue);
         }
     }
 
-    window.customElements.define("x-clock", XClock);
-
+    window.customElements.define('x-clock', XClock);
 })();
